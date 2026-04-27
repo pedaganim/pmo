6343d5752355e90207cd3a641da59d8094573cbb
pedaganim <madhukar.pedagani@gmail.com>
2026-04-27 13:15:55 +1000

    Declare tenant_id and domain_name in root terraform variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,3 +3,14 @@ variable "aws_region" {
   type        = string
   default     = "ap-southeast-2"
 }
+
+variable "tenant_id" {
+  description = "The unique ID for the tenant"
+  type        = string
+}
+
+variable "domain_name" {
+  description = "The custom domain for the tenant"
+  type        = string
+}
+
